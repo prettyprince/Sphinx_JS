17e740715e703dd125933db2528eb828b644cd19
prettyprince <bobosusu88@yahoo.com>
2015-10-20 11:43:27 +0800

    Initial Commit

---
+++ SFK student center/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SFK student center
+//
+//  Created by Jeemy on 15/10/20.
+//  Copyright © 2015年 SKF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SFK student center/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SFK student center
+//
+//  Created by Jeemy on 15/10/20.
+//  Copyright © 2015年 SKF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
